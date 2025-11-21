430921719739ff74ffeaf1162cff9611bd42f821
Ucha Kobakhidze <uchakobakhidze@gmail.com>
2025-11-22 02:21:37 +0400

    added /gallery stylings

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,5 +1,6 @@
 import { getImages } from '@/app/actions';
 import InteractiveGallery from '@/components/Gallery/InteractiveGallery';
+import ParallaxText from '@/components/Gallery/ParallaxText';
 import { verifySession } from '@/lib/auth';
 
 export default async function GalleryPage() {
@@ -8,23 +9,31 @@ export default async function GalleryPage() {
   const isAdmin = await verifySession();
 
   return (
-    <div className="min-h-screen bg-[var(--color-background)] text-black font-[Gilroy]">
-      <main className="max-w-7xl mx-auto py-20">
-        <header className="mb-16 text-center">
-          <h1 className="text-5xl font-bold text-black mb-4">
-            Image Gallery
-          </h1>
-          <p className="text-[var(--color-accent)] text-lg font-medium">
-            {isAdmin ? 'Welcome back, Tamar.' : 'Browse my collection.'}
-          </p>
-        </header>
-
-        {/* 3. Pass data to Client Component */}
-        <InteractiveGallery 
-          images={images} 
-          isAdmin={isAdmin} 
-        />
-      </main>
-    </div>
+    <main className="w-full py-20">
+            <section className='py-20'>
+                <ParallaxText baseVelocity={-5}>
+                memories &nbsp; memories &nbsp; memories &nbsp; memories &nbsp;
+                </ParallaxText>
+                <ParallaxText baseVelocity={5}>
+                Explore &nbsp; Explore &nbsp; Explore &nbsp; Explore &nbsp; Explore &nbsp;
+                </ParallaxText>
+            </section>
+        <div className='max-w-7xl mx-auto'>
+            <section className='mx-auto w-full'>
+                <div className="mb-16 text-center">
+                <h1 className="text-5xl font-medium mb-4 uppercase">
+                    Image Gallery
+                </h1>
+                <p className="text-[var(--color-accent)] text-lg font-medium">
+                    {isAdmin ? 'Welcome back, Tamar.' : 'Browse my collection.'}
+                </p>
+                </div>
+                <InteractiveGallery 
+                images={images} 
+                isAdmin={isAdmin} 
+                />
+            </section>
+        </div>
+    </main>
   );
 }
